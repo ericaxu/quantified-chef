d90c7b3fc05415dc9e1171657ee029601069ee7d
Erica Xu <iam@ericaxu.com>
2019-05-08 00:24:58 -0400

    Init commit

---
+++ data.js
@@ -0,0 +1,25 @@
+let dishes = [
+    {
+        english_name: 'Test',
+        chinese_name: '测试',
+        english_notes: 'None',
+        chinese_notes: '无',
+        category: 'traditional_chinese',
+        date: '2018-03-01',
+        image: 'https://www.onceuponachef.com/images/2010/09/Summer-Corn-Soup-850x1063.jpg'
+    },
+    {
+        english_name: 'Test',
+        chinese_name: '测试',
+        english_notes: 'None',
+        chinese_notes: '无',
+        category: 'traditional_chinese'
+    }
+];
+
+let categories = {
+    'traditional_chinese': {
+        english_name: 'Traditional Chinese',
+        chinese_name: '传统中餐'
+    }
+};
---
+++ render.js
@@ -0,0 +1,70 @@
+function create_el(tag, parent, class_name, text) {
+    if (!tag) {
+        return;
+    }
+
+    let new_element = document.createElement(tag);
+
+    if (class_name) {
+        new_element.className = class_name;
+    }
+
+    if (text) {
+        new_element.textContent = text;
+    }
+
+    if (parent) {
+        parent.appendChild(new_element);
+    }
+
+    return new_element;
+}
+
+function render_categories(container) {
+    for (let category in categories) {
+        let id = category;
+        let category_data = categories[id];
+
+        let category_el = create_el('div', container, 'category');
+        category_el.setAttribute('data-id', id);
+        let category_header_el = create_el('div', category_el, 'category-header');
+
+        create_el('span', category_header_el, 'category-english-name', category_data.english_name || '');
+        create_el('span', category_header_el, 'category-chinese-name', category_data.chinese_name || '');
+
+        create_el('div', category_el, 'category-dishes-container');
+    }
+}
+
+function render_dishes() {
+    for (let dish of dishes) {
+        let category_id = dish.category;
+
+        if (categories[category_id]) {
+            let dishes_container = document.querySelector(`.category[data-id="${category_id}"] > .category-dishes-container`);
+
+            if (!dishes_container) {
+                return;
+            }
+
+            let dish_el = create_el('div', dishes_container, 'dish');
+            let dish_header_el = create_el('div', dish_el, 'dish-header');
+
+            create_el('span', dish_header_el, 'dish-english-name', dish.english_name || '');
+            create_el('span', dish_header_el, 'dish-chinese-name', dish.chinese_name || '');
+
+            let image_el = create_el('img', dish_el, 'dish-image');
+
+            if (dish.image) {
+                image_el.setAttribute('src', dish.image);
+            }
+            else {
+                image_el.setAttribute('src', 'placeholder.png');
+            }
+        }
+        else {
+            console.error('Cannot find category: ' + category_id);
+        }
+
+    }
+}
