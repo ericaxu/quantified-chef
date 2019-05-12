7b22c93b88c64208cb572593fa99af56dcee06cf
Erica Xu <iam@ericaxu.com>
2019-05-11 21:49:31 -0400

    New layout

--- render.js
+++ render.js
@@ -23,18 +23,26 @@ function create_el(tag, parent, class_name, text) {
     return new_element;
 }
 
-function sort_dishes() {
-    new_dishes = [];
-
+function render_categories(container) {
     for (let category in categories) {
-        for (let dish of dishes) {
-            if (dish.category === category) {
-                new_dishes.push(dish);
-            }
-        }
-    }
+        let id = category;
+        let category_data = categories[id];
 
-    dishes = new_dishes;
+        let category_el = create_el('div', container, 'category');
+        category_el.setAttribute('data-id', id);
+        let category_header_el = create_el('div', category_el, 'category-header');
+
+        create_el('span', category_header_el, 'category-english-name', category_data.english_name || '');
+        create_el('span', category_header_el, 'category-chinese-name', category_data.chinese_name || '');
+
+        let progress_el = create_el('div', category_header_el, 'category-progress');
+        progress_el.setAttribute('data-all', '0');
+        progress_el.setAttribute('data-made', '0');
+        create_el('div', progress_el, 'category-progress-inner');
+        create_el('div', progress_el, 'tooltip');
+
+        create_el('div', category_el, 'category-dishes-container');
+    }
 }
 
 function render_dishes(container_el) {
@@ -46,42 +54,20 @@ function render_dishes(container_el) {
         let category_id = dish.category;
 
         if (categories[category_id]) {
-            let dish_el = create_el('div', container_el, 'dish');
-            let category_data = categories[category_id];
+            let dishes_container = document.querySelector(`.category[data-id="${category_id}"] > .category-dishes-container`);
 
-            let category_el = create_el('div', dish_el, 'category');
-            category_el.setAttribute('data-id', category_id);
-
-            let category_header_el = create_el('div', category_el, 'category-header');
-
-            create_el('span', category_header_el, 'category-english-name', category_data.english_name || '');
-            create_el('span', category_header_el, 'category-chinese-name', category_data.chinese_name || '');
-
-            let existing_progress_els = document.querySelectorAll(`.category-progress[data-id="${category_id}"]`);
-
-            let progress_el = create_el('div', category_header_el, 'category-progress');
-            if (existing_progress_els.length === 0) {
-                progress_el.setAttribute('data-all', '0');
-                progress_el.setAttribute('data-made', '0');
-            }
-            else {
-                progress_el.setAttribute('data-all', existing_progress_els[0].getAttribute('data-all'));
-                progress_el.setAttribute('data-made', existing_progress_els[0].getAttribute('data-made'));
+            if (!dishes_container) {
+                return;
             }
 
-            progress_el.setAttribute('data-id', category_id);
-            create_el('div', progress_el, 'category-progress-inner');
-            create_el('div', category_el, 'category-dishes-container');
-            let progress_tooltip_el = create_el('div', progress_el, 'tooltip');
-            progress_tooltip_el.setAttribute('data-title', '');
-
-            let dish_inner_el = create_el('div', dish_el, 'dish-inner');
-            let dish_header_el = create_el('div', dish_inner_el, 'dish-header');
+            let progress_el = document.querySelector(`.category[data-id="${category_id}"] .category-progress`);
+            let dish_el = create_el('div', dishes_container, 'dish');
+            let dish_header_el = create_el('div', dish_el, 'dish-header');
 
             create_el('span', dish_header_el, 'dish-english-name', dish.english_name || '');
             create_el('span', dish_header_el, 'dish-chinese-name', dish.chinese_name || '');
 
-            let image_container_el = create_el('div', dish_inner_el, 'dish-image-container');
+            let image_container_el = create_el('div', dish_el, 'dish-image-container');
             let image_el = create_el('img', image_container_el, 'dish-image');
 
             if (dish.image) {
@@ -96,43 +82,38 @@ function render_dishes(container_el) {
                 image_el.setAttribute('src', 'placeholder.png');
             }
 
-            for (let el of [...existing_progress_els, progress_el]) {
-                el.setAttribute('data-all', (parseInt(el.getAttribute('data-all')) + 1).toString());
-            }
+            progress_el.setAttribute('data-all', (parseInt(progress_el.getAttribute('data-all')) + 1).toString());
 
             if (!dish.date) {
                 dishes_to_make_count += 1;
-                dish_inner_el.className += ' mod-not-completed';
+                dish_el.className += ' mod-not-completed';
             }
             else {
                 dishes_made_count += 1;
-                for (let el of [...existing_progress_els, progress_el]) {
-                    el.setAttribute('data-made', (parseInt(el.getAttribute('data-made')) + 1).toString());
-                }
+                progress_el.setAttribute('data-made', (parseInt(progress_el.getAttribute('data-made')) + 1).toString());
+
                 let date = new Date(dish.date + ' EST');
                 if (date.getTime() > last_updated_ts) {
                     last_updated_ts = date.getTime();
                 }
 
-                create_el('div', dish_inner_el, 'dish-made-date', date.toLocaleDateString());
+                create_el('div', dish_el, 'dish-made-date', date.toLocaleDateString());
             }
 
             if (dish.english_notes) {
-                create_el('div', dish_inner_el, 'dish-english-notes', dish.english_notes);
+                create_el('div', dish_el, 'dish-english-notes', dish.english_notes);
             }
 
             if (dish.chinese_notes) {
-                create_el('div', dish_inner_el, 'dish-chinese-notes', dish.chinese_notes);
+                create_el('div', dish_el, 'dish-chinese-notes', dish.chinese_notes);
             }
 
             let all_dishes_count = parseInt(progress_el.getAttribute('data-all'));
             let made_dishes_count = parseInt(progress_el.getAttribute('data-made'));
             let percentage = ((made_dishes_count / all_dishes_count) * 100).toString() + '%';
 
-            for (let el of [...existing_progress_els, progress_el]) {
-                el.querySelector('.tooltip').setAttribute('data-title', `${made_dishes_count}/${all_dishes_count}, ${percentage} done`);
-                el.querySelector('.category-progress-inner').style.width = percentage;
-            }
+            progress_el.querySelector('.tooltip').setAttribute('data-title', `${made_dishes_count}/${all_dishes_count}, ${percentage} done`);
+            progress_el.querySelector('.category-progress-inner').style.width = percentage;
 
         }
         else {
